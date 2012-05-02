d1cee0d3df19d47647364a3f35280074aed952b2
Dirk <coolice.f.b@web.de>
2012-05-02 14:40:45 +0200

    adc.c

--- MDV PR 2/C/adc.c
+++ MDV PR 2/C/adc.c
@@ -1,4 +1,4 @@
-*
+ /*
  * \addtogroup adc
  *
  * \file adc.c
@@ -16,24 +16,32 @@
 
 
 /*** GLOBAL VARIABLES ***/
+// wir initialisieren eine 32-Bit Zählvariable um später die Anzahl der noch durchzuführenden Wandlungen zu speichern.
 uint32_t SAMPLE_COUNTER = 0;
 
 
 /*** INTERRUPT ROUTINES ***/
 ISR(TIMER1_COMPA_vect) {
-
-
 }
 
 ISR(TIMER1_COMPB_vect) {
 }
 
+// dieser interrupt wird ausgeführt, wenn der ADU mit einer umsetzung feritg ist
 ISR(ADC_vect) {
 
+	// der Wert des ADU wird ausgelesen
 	uint16_t adu_wert = ADC; //ADCH und ADCL werden gemeinsam als 16-bit Wert ausgelesen
+
+	// Speichern des ADU-Wertes in einem Buffer
 	filterWrite2Buf(adu_wert -512) ;
 
+	// die in adcStart gesetzte Anzahl an Umsetzungen wird decrementiert
 	SAMPLE_COUNTER--;
+
+	// Überprüugn ob noch Umsetzungen übrig sind. Falls nicht, werden die Interrups
+	// ausgeschaltet, sodass diese Funktion nicht weiter aufgerufen wird.
+	//(der ADU läuft aber weiter)
 	if (SAMPLE_COUNTER == 0){
 		cli();
 	}
@@ -61,33 +69,38 @@ void adcInit() {
 void adcStart(uint16_t sampleRateCode, uint32_t sampleCount, trigger_t triggerMode, int16_t triggerLevel) {
 	cli();
 
-	SAMPLE_COUNTER = sampleCount; //sampleCounter auf den gewünschten wert setzen
+	//sampleCounter auf den gewünschten wert setzen
+	SAMPLE_COUNTER = sampleCount;
 
-	OCR1A =  sampleRateCode; // die semplerate auf den richtigen wert setzen
+	// die semplerate auf den übergebenen wert setzen
+	OCR1A =  sampleRateCode;
+
+	// Clock auf non-prescaling, betrieben mit CPU-Takt
+	TCCR1B |=(1<<CS10);
+
+	//CTC Mode
+	TCCR1B |=(1<<WGM12);
 
 
-	TCCR1B |=(1<<CS10); // Clock auf non-prescaling, betrieben mit CPU-Takt
-	TCCR1B |=(1<<WGM12); //CTC Mode
-
-
-
+	// Timer startet bei 0
 	TCNT1 = 0 ;
-	// Timer s t a r t e t b e i 0
 
+	// Timer Output Compare Interrupt B aktivieren
 	TIMSK1 = (1<<OCIE1A);
-	// Timerâˆ’Outputâˆ’Compareâˆ’I n t e r r u p t B a k t i v i e r e n
 
+	// kein Output/Compare
 	TCCR1A = 0;
-	// k e i n Output /Compare
 
+	// Interrupts anschalten, damit die umgesetzten Werte gespeichert werden
 	sei();
 }
 
 uint8_t adcIsRunning() {
 
-if(SAMPLE_COUNTER==0){
-	return 0;
+	// Überprüfung ob aktuell noch umsetzungen gemscht werden müssen
+	if(SAMPLE_COUNTER==0){
+		return 0;
 	} else {
-	return 1;
+		return 1;
 	}
 }
